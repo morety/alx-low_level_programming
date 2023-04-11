32145f4671eaf55615c04012f05a0ab9448a4e79
morety <muretisammy@gmail.com>
2023-04-10 19:02:18 -0700

    static libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_puts - prints a string
+ *@str: function para
+ *Return: 0
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,2 @@
+#Project Static libraries
+
